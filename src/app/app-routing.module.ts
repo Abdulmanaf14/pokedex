2e2aa39be6e1ed604bb95e6f2ee51993883b39d6
abdul manaf <abdul.manaf@fedo.ai>
2023-12-07 21:48:39 +0530

    telp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'pokedex' ,pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'contactus',component:ContactusComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'pokedex',component:PokedexComponent},
+  {path:'pokemon-details',component:PokemonDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
